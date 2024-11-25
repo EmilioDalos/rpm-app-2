4c0dfb908251ecfa56fb5841debc0dd77ed70c63
EmilioDalos <39067540+EmilioDalos@users.noreply.github.com>
2024-11-25 18:14:58 +0100

    make all fases work

--- app/capturelist/page.tsx
+++ app/capturelist/page.tsx
@@ -38,12 +38,13 @@ export default function Page() {
       <div className="container mx-auto p-4">
       
           <div className="grid gap-8 md:grid-cols-2">
+          <div>
+              <Capturelist />
+            </div>
             <div>
               <RpmOverview blocks={blocks} />
             </div>
-            <div>
-              <Capturelist />
-            </div>
+           
           </div>
         
     </div>
